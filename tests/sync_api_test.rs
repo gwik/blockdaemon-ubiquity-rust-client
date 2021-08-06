d9f71eed3186f477565088884fd636c848bc2e65
Gabriel Crispino <gabriel@blockdaemon.com>
2021-08-06 10:19:13 -0300

    Refactor sync api tests

--- tests/sync_api_test.rs
+++ tests/sync_api_test.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{future::Future, io};
 
 use ubiquity::api::sync_api;
 use ubiquity::configuration;
@@ -51,46 +51,53 @@ fn config_from_url(url: String) -> configuration::Configuration {
   };
 }
 
-#[tokio::test]
-async fn sync_block_number() {
+async fn sync_test<F, Fut>(func: F)
+where
+  F: Fn(configuration::Configuration) -> Fut,
+  Fut: Future<Output = ()>,
+{
   match setup() {
     Ok(setup_data) => {
       let _m = setup_data.mocks;
 
       let config = config_from_url(setup_data.url);
 
-      let test_platform_pairs = [("bitcoin", "mainnet"), ("ethereum", "mainnet")];
-
-      for (platform, network) in test_platform_pairs {
-        let res = sync_api::current_block_number(&config, platform, network).await;
-        match res {
-          Ok(_) => {}
-          Err(e) => panic!("{}", e),
-        };
-      }
+      func(config).await;
     }
     Err(e) => panic!("{}", e),
   }
 }
 
+async fn test_current_block_id_with_config(config: configuration::Configuration) {
+  let test_platform_pairs = [("bitcoin", "mainnet"), ("ethereum", "mainnet")];
+
+  for (platform, network) in test_platform_pairs {
+    let res = sync_api::current_block_id(&config, platform, network).await;
+    match res {
+      Ok(_) => {}
+      Err(e) => panic!("{}", e),
+    };
+  }
+}
+
+async fn test_current_block_number_with_config(config: configuration::Configuration) {
+  let test_platform_pairs = [("bitcoin", "mainnet"), ("ethereum", "mainnet")];
+
+  for (platform, network) in test_platform_pairs {
+    let res = sync_api::current_block_number(&config, platform, network).await;
+    match res {
+      Ok(_) => {}
+      Err(e) => panic!("{}", e),
+    };
+  }
+}
+
 #[tokio::test]
 async fn sync_block_id() {
-  match setup() {
-    Ok(setup_data) => {
-      let _m = setup_data.mocks;
+  sync_test(test_current_block_id_with_config).await;
+}
 
-      let config = config_from_url(setup_data.url);
-
-      let test_platform_pairs = [("bitcoin", "mainnet"), ("ethereum", "mainnet")];
-
-      for (platform, network) in test_platform_pairs {
-        let res = sync_api::current_block_id(&config, platform, network).await;
-        match res {
-          Ok(_) => {}
-          Err(e) => panic!("{}", e),
-        };
-      }
-    }
-    Err(e) => panic!("{}", e),
-  }
+#[tokio::test]
+async fn sync_block_number() {
+  sync_test(test_current_block_number_with_config).await;
 }
